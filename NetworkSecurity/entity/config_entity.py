a3bc20057019231ac0b6758f13aae24eaf26bf73
P.Ramsai <ponugotiramsai@gmail.com>
2026-09-11 07:05:34 +0530

    main.py

---
+++ NetworkSecurity/entity/config_entity.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import os
+
+from NetworkSecurity.constants import trainingpipeline
+
+print(trainingpipeline.PIPELINE_NAME)
+print(trainingpipeline.ARTIFACT_DIR)
+
+
+class TrainingPipelineConfig:
+    def __init__(self,timestamp=datetime.now()):
+        timestamp=timestamp.strftime("%m_%d_%Y_%H_%M_%S")
+        self.pipeline_name=trainingpipeline.PIPELINE_NAME
+        self.artifact_name=trainingpipeline.ARTIFACT_DIR
+        self.artifact_dir=os.path.join(self.artifact_name,timestamp)
+        self.model_dir=os.path.join("final_model")
+        self.timestamp: str=timestamp
+
+
+
+class DataIngestionConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_ingestion_dir:str=os.path.join(
+            training_pipeline_config.artifact_dir,trainingpipeline.DATA_INGESTION_DIR_NAME
+        )
+        self.feature_store_file_path: str = os.path.join(
+                self.data_ingestion_dir, trainingpipeline.DATA_INGESTION_FEATURE_STORE_DIR, trainingpipeline.FILE_NAME
+            )
+        self.training_file_path: str = os.path.join(
+                self.data_ingestion_dir, trainingpipeline.DATA_INGESTION_INGESTED_DIR, trainingpipeline.TRAIN_FILE_NAME
+            )
+        self.testing_file_path: str = os.path.join(
+                self.data_ingestion_dir, trainingpipeline.DATA_INGESTION_INGESTED_DIR, trainingpipeline.TEST_FILE_NAME
+            )
+        self.train_test_split_ratio: float = trainingpipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
+        self.collection_name: str = trainingpipeline.DATA_INGESTION_COLLECTION_NAME
+        self.database_name: str = trainingpipeline.DATA_INGESTION_DATABASE_NAME
---
+++ main.py
@@ -0,0 +1,17 @@
+from NetworkSecurity.components.data_ingestion import DataIngestion
+from NetworkSecurity.exception.exception import NetworkSecurityException
+from NetworkSecurity.logging.logger import logging
+from NetworkSecurity.entity.config_entity import DataIngestionConfig
+from NetworkSecurity.entity.config_entity import TrainingPipelineConfig
+import sys
+
+if __name__=='__main__':
+    try:
+         trainingPipelineConfig=TrainingPipelineConfig()
+         dataingestionconfig=DataIngestionConfig(trainingPipelineConfig)
+         data_ingestion=DataIngestion(dataingestionconfig)
+         logging.info("initiate the data ingestion")
+         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+         print(dataingestionartifact)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
